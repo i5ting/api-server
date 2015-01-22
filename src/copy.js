b0e3e04a81816f2b5d28333f85bcf70ce4fd7e2d
alfred sang <i5ting@126.com>
2015-01-22 18:35:35 +0800

    copy fix

--- src/copy.js
+++ src/copy.js
@@ -8,18 +8,26 @@ path_arr.pop();
 
 npm_installed_path = path_arr.join("/");
 
-
+// only for cli test
 var desc_path = process.cwd() + '/node_modules';
 
-var sources = ['supervisor','shelljs','node-require-directory',"express","body-parser","cookie-parser","morgan","serve-favicon","debug","jade" ]
+var sources_npm = ['supervisor','shelljs','node-require-directory',"express","body-parser","cookie-parser","morgan","serve-favicon","debug","jade" ]
 
-function main(){
-	sources.forEach(function(folder_name){
-		var source_path = _get_path_with(folder_name);
-		_cp(source_path,desc_path);
+
+function main(desc_path){
+	var desc_path_npm = desc_path + '/node_modules';
+	// npm copy
+	sources_npm.forEach(function(folder_name){
+		var source_path = _get_npm_path_with(folder_name);
+		_cp(source_path, desc_path_npm);
 	});
 	
+	// npm/.bin copy
 	cp_bin();
+	
+	// vendor copy
+	var source_path_vendor = _get_vendor_path();
+	_cp(source_path_vendor, desc_path);
 }
 
 function cp_bin(){
@@ -29,8 +37,12 @@ function cp_bin(){
 	cp('-Rf', source_path, desc_path);
 }
 
-function _get_path_with(folder_name){
+function _get_npm_path_with(folder_name){
 	return npm_installed_path + '/node_modules/' + folder_name;
+}
+
+function _get_vendor_path(){
+	return npm_installed_path + '/vendor';
 }
 
 function _cp(source_path,desc_path){
@@ -38,7 +50,7 @@ function _cp(source_path,desc_path){
 }
 
 if( process.argv.length > 1){
-	main();
+	main(desc_path);
 }
 
 module.exports = main;
